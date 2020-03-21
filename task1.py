9172ad4595cc8efdcee75e18cadde12d1cb69f50
Ayush Jangir <jangirayush59@gmail.com>
2020-03-21 14:20:52 +0530

    Update the file for Task 1

--- task1.py
+++ task1.py
@@ -1,12 +1,7 @@
 import requests
 
-<<<<<<< HEAD
-res = requests.get('https://www.theverge.com/tech')
-f = open('site.html', 'w')
-=======
 res = requests.get('https://www.theverge.com/tech') # Obtain the source of the webpage, and
 f = open('site.html', 'w')                          # write it to a local file.
->>>>>>> 60aaa06be48eede974a27b9f4fd002cad6bea5c4
 f.write(res.text)
 
 def lst(string):
@@ -19,22 +14,13 @@ def lst(string):
 
     return l
 
-<<<<<<< HEAD
-f = open('site.html', 'r')
-=======
 f = open('site.html', 'r')                         # Read and interpret the generated local file.
->>>>>>> 60aaa06be48eede974a27b9f4fd002cad6bea5c4
 lines = f.readlines()
 i = 0
 print("\nToday's headlines from the Tech section of the website of The Verge:-\n")
 for line in lines:
-<<<<<<< HEAD
-    if (line[:44] == '    <h2 class="c-entry-box--compact__title">'):
-        i += 1
-=======
     if (line[:44] == '    <h2 class="c-entry-box--compact__title">'):   # All headlines on this page follow this
         i += 1                                                          # condition in the source code.
->>>>>>> 60aaa06be48eede974a27b9f4fd002cad6bea5c4
         ls = lst(line)
         print('%d: %s' % (i, ls[len(ls) - 5]))
-print('\n')
+print('')
